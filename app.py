7a0cd710544821b8fb6ff683ce54f8205713f6ec
Wade Fletcher <wbfletch@gmail.com>
2020-12-19 12:33:27 -0500

    rearrange to single app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Report Submission')
+
+
+if __name__ in '__main__':
+    app.run(DEBUG=True)
