3f03bb79b1897397932adf4687da4c66238a07ee
Wade Fletcher <wbfletch@gmail.com>
2020-12-19 14:29:26 -0500

    rewrite fake.py

--- fake.py
+++ fake.py
@@ -1,13 +1,15 @@
-from sqlfaker.database import Database
+from app import db, Report, Team
 
-db = Database(db_name='app')
+NUM_REPORTS = 40
 
-db.tables["app"].add_primary_key(column_name="student_id")
-db.tables["app"].add_column(column_name="username", data_type="varchar(50)", data_target="name")
-db.tables["app"].add_column(column_name="title", data_type="varchar(50)", data_target="name")
-db.tables["app"].add_column(column_name="ticker", data_type="varchar(4)", data_target="file")
-db.tables["app"].add_column(column_name="date", data_type="date", data_target="date")
-db.tables["app"].add_column(column_name="team", data_type="varchar(30", data_target="job")
+db.session.add(
+    Team(name='', manager='', color='')
+)
 
-db.generate_data()
-db.export_sql("app.sql")
+db.session.commit()
+
+for x in range(NUM_REPORTS):
+    db.session.add(Report(
+        'args'
+    ))
+db.session.commit()
