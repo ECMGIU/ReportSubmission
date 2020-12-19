50827ffd74534054941d3bd83c0f7e7156fdcba9
Wade Fletcher <3798059+wadefletch@users.noreply.github.com>
2020-12-19 14:35:00 -0500

    Update fake.py

--- fake.py
+++ fake.py
@@ -3,12 +3,12 @@ from app import db, Report, Team
 NUM_REPORTS = 40
 
 db.session.add(
-    Team(name='Domestic Equity', manager='pwgould', color='Red'),
-    Team(name='International Equity', manager='spsahoo', color='Yellow'),
-    Team(name='Sustainable Investing', manager='madamann', color='Green'),
-    Team(name='Commodities', manager='sahmehta', color='Purple'),
-    Team(name='Real Estate', manager='evanhunt', color='Orange'),
-    Team(name='Macro Research', manager='ypande', color='Blue')
+    Team(name='Domestic Equity', manager='pwgould', color='red'),
+    Team(name='International Equity', manager='spsahoo', color='yellow'),
+    Team(name='Sustainable Investing', manager='madamann', color='green'),
+    Team(name='Commodities', manager='sahmehta', color='purple'),
+    Team(name='Real Estate', manager='evanhunt', color='orange'),
+    Team(name='Macro Research', manager='ypande', color='blue')
 )
 
 db.session.commit()
