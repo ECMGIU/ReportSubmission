4dc2c2021b277400dc5b9c7a940ee9e76c1306e7
sarnagar12 <74271614+sarnagar12@users.noreply.github.com>
2020-12-19 14:34:33 -0500

    Update fake.py

--- fake.py
+++ fake.py
@@ -3,7 +3,12 @@ from app import db, Report, Team
 NUM_REPORTS = 40
 
 db.session.add(
-    Team(name='', manager='', color='')
+    Team(name='Domestic Equity', manager='pwgould', color='Red'),
+    Team(name='International Equity', manager='spsahoo', color='Yellow'),
+    Team(name='Sustainable Investing', manager='madamann', color='Green'),
+    Team(name='Commodities', manager='sahmehta', color='Purple'),
+    Team(name='Real Estate', manager='evanhunt', color='Orange'),
+    Team(name='Macro Research', manager='ypande', color='Blue')
 )
 
 db.session.commit()
