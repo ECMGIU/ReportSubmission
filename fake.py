639b53e0854a60c495485d36b29079c52f0b43a3
Wade Fletcher <wbfletch@gmail.com>
2020-12-19 14:42:22 -0500

    added Report faking

--- fake.py
+++ fake.py
@@ -1,6 +1,11 @@
+import random
+
 from app import db, Report, Team
+from faker import Faker
 
 NUM_REPORTS = 40
+
+fake = Faker()
 
 db.session.add(
     Team(name='', manager='', color='')
@@ -10,6 +15,10 @@ db.session.commit()
 
 for x in range(NUM_REPORTS):
     db.session.add(Report(
-        'args'
+        username=fake.pystr(8, 8).lower(),
+        title=fake.sentence(7),
+        ticker=fake.pystr(4, 4).upper(),
+        date=fake.date_this_month(),
+        team=random.choice(db.session.query(Team))
     ))
 db.session.commit()
