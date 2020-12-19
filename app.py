20860539afd60f0265807fc74df516abf9762a8e
Wade Fletcher <wbfletch@gmail.com>
2020-12-19 12:48:47 -0500

    database boilerplate

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+import os
 
+from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Report Submission')
+    return render_template('index.html')
 
 
 if __name__ in '__main__':
